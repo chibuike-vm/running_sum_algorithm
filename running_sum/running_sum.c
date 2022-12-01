7d86120a31bbfbec94f5206fc43e22f39bedc4d2
Chibuike Mba <chibuike.code@gmail.com>
2022-12-01 22:26:07 +0100

    Refactor running_sum.c
    This commit will refactor the mathematical algorithm for the running sum
    of 5 positive integers inputs using a different style from the previous
    commit. I used "for loop" and if-else conditional statements. Also, I
    intentionally used the "break" loop execution flow control statement
    inside the block scope of the "else" statement in the "for loop" just to
    test its functionality and performance with the algorithm.

--- running_sum/running_sum.c
+++ running_sum/running_sum.c
@@ -3,25 +3,24 @@
 int main(void) {
 	int i = 0, sum = 0, a;
 	printf("kindly type in the positive integer you want to sum: ");
-
-	while (i >= 0 && i <= 4) {
-		if (i >= 1) {
-			printf("Kindly type in another positive integer you want to sum: ");
+	 
+	for (i; i <= 4; i++) {
+		if (i == 0) {
+			scanf("%d", &a);
+		} else {
+			printf("kindly type in another positive integer you want to sum: ");
+			scanf("%d", &a);
 		}
-		
-		scanf("%d", &a);
 
-		if (a >= 0 ) {
+
+		if (a >= 0) {
 			sum += a;
 		} else {
-			goto next_action;		
+			break;
 		}
-
-		i++;
 	}
 
-	next_action:
-		printf("The total sum of the %d positive integer input(s) is = %d\n", i, sum);
-
+	printf("The total sum of %d positive integer input(s) is = %d\n\n", i, sum);
+           
 	return 0;
 }
